bf55f305a0e1c4293f28feb08346ce747105747e
Matias Gonzalez <gonzalezmatias349ggmail.com>
2022-07-31 00:37:03 -0400

    feat functions

---
+++ assets/js/array.js
@@ -0,0 +1,14 @@
+export let initialTasks = [
+    {
+     id: 1,
+     task: 'agregar tareas con descripcion al llenar el input'
+    },
+    {
+     id: 2,
+     task: 'borrar tareas al hacer click en borrar'
+    },
+    {
+     id: 3,
+     task: 'incluir 3 tareas iniciales'
+    },
+]
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,72 @@
-const taskList = document.querySelector("")
-const newTask = document.querySelector("")
-const addTask = document.querySelector("")
-const tasks = []
+// import { initialTasks } from './array.js'
+const taskList = document.querySelector("#taskList")
+        const inputTask = document.querySelector("#inputTask")
+        const addTask = document.querySelector("#addTask")
+        const total = document.querySelector("#total")
+        const complete = document.querySelector("#completed")
+        let tasks = [
+            {id: 1,task: 'agregar tareas con descripcion al llenar el input',state: false},
+            {id: 2,task: 'borrar tareas al hacer click en borrar',state: false},
+            {id: 3,task: 'incluir 3 tareas iniciales',state: false}]
 
+        addTask.addEventListener("click", () => {
+            if(inputTask.value ===""){
+                alert("Debes agregar una tarea primero")
+                return
+            }
+            else{
+                const newTask = {id:Date.now() ,task:inputTask.value,state:false}
+                tasks.push(newTask)
+                inputTask.value = ""
+                let html = ""
+    
+                for (let task of tasks){
+                    html += `<li>${task.task} <button onclick="completed(${task.id})" class="finished">completada</button> <button onclick="Borrar(${task.id})" class="delete">Borrar</button></li>`;
+                }
+                taskList.innerHTML = html;
+                console.log(tasks)
+            }
+        })
+        function Borrar(id){
+            const index = tasks.findIndex((ele) => ele.id == id)
+            tasks.splice(index, 1)
+            let html = ""
+            for (task of tasks){
+                html += `<li>${task.task} <button onclick="completed(${task.id})" id="${task.id}" class="finished">completada</button> <button onclick="Borrar(${task.id})" class="delete">Borrar</button></li>`;             
+            }
+            taskList.innerHTML = html;
+            console.log(tasks)
+        }
+        const initialTask = function(){
+            let html = ""
+    
+            for (let task of tasks){
+                html += `<li>${task.task} <button onclick="completed(${task.id})" id="${task.id}" class="finished">completada</button> <button onclick="Borrar(${task.id})" class="delete">Borrar</button></li>`;
+            }
+            taskList.innerHTML = html;
+            console.log("tasks")
 
-
-
-
-
-
-
-
-const listaDeInvitados = document.querySelector("#invitados")
-const invitadoInput = document.querySelector("#nuevoInvitado")
-const btnAgregar = document.querySelector("#agregarInvitado")
-const invitados = []
-btnAgregar.addEventListener("click", () => {
-/* Agregamos el invitado al arreglo */
-const nombreInvitado = invitadoInput.value
-invitados.push(nombreInvitado)
-invitadoInput.value = ""
-/* Actualizamos la información en el HTML */
-let html = ""
-for (let invitado of invitados) {
-html += `<li>${invitado}</li>`;
-}
-listaDeInvitados.innerHTML = html;
-})
+        }
+        const color = function(id){
+            let color = "yellow"
+            let okbutton = document.getElementById(id)
+            okbutton.style.backgroundColor = color
+            console.log(id)
+            console.log(document.getElementById(id).style)
+        }
+        function completed(id){
+            const index = tasks.findIndex((ele) => ele.id == id)
+            let html = ""
+            if(tasks[index].state === true){
+                tasks[index].state = false
+                color(id)
+            }else{
+                tasks[index].state = true
+            }
+            for (task of tasks){
+                html += `<li>${task.task} <button onclick="completed(${task.id})" id="${task.id}" class="finished">completada</button> <button onclick="Borrar(${task.id})" class="delete">Borrar</button></li>`;             
+            }
+            taskList.innerHTML = html;
+            console.log(tasks)
+        }
+        initialTask()
